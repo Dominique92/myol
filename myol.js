618c482bb5c81c95dac84a4bd52d0b6a75c5e51e
Dominique Cavailhez <protec@cavailhez.fr>
2018-10-20 18:37:08 +0200

    Nouveaux types OSM overpass

--- myol.js
+++ myol.js
@@ -529,7 +529,6 @@ ol.inherits(ol.format.OSMXMLPOI, ol.format.OSMXML);
  * Doc: http://wiki.openstreetmap.org/wiki/Overpass_API/Language_Guide
  * Requires layerVectorURL
  */
-//BEST restos, wc
 function layerOverpass(options) {
 	var layer = layerVectorURL({
 		url: function(bbox, list, resolution) {
@@ -555,7 +554,7 @@ function layerOverpass(options) {
 		style: function(properties) {
 			return {
 				image: new ol.style.Icon({
-					src: options.iconUrl + overpassType(properties).icon + '.png'
+					src: options.iconUrl + overpassType(properties) + '.png'
 				})
 			};
 		},
@@ -563,6 +562,7 @@ function layerOverpass(options) {
 		label: formatLabel
 	});
 
+	//TODO : afficher erreur 429 (Too Many Requests)
 	function formatLabel(p, f) { // p = properties, f = feature
 		var language = {
 				hotel: 'h&ocirc;tel',
@@ -570,46 +570,75 @@ function layerOverpass(options) {
 				convenience: 'alimentation',
 				supermarket: 'supermarch&egrave;',
 				drinking_water: 'point d\'eau',
-				water_point: 'point d\'eau',
 				watering_place: 'abreuvoir',
-				spring: 'source',
-				water_well: 'puits',
 				fountain: 'fontaine'
 			},
-			type = overpassType(p).name,
-			description = [
-				(!p.name ? type : (p.name.toLowerCase().indexOf(type) ? type : '')), //TODO : devrait utiliser la traduction
-				'*'.repeat(p.stars),
-				p.rooms ? p.rooms + ' chambres' : '',
-				p.place ? p.place + ' places' : '',
-				p.capacity ? p.capacity + ' places' : '',
-				p.ele ? parseInt(p.ele, 10) + 'm' : '',
-			].join(' ').replace( // Word translation if necessary
-				new RegExp(Object.keys(language).join('|'), 'gi'),
-				function(m) {
-					return language[m.toLowerCase()];
-				}
-			),
 			phone = p.phone || p['contact:phone'],
 			address = [
-				p['addr:housenumber'],
-				p['addr:street'],
-				p['addr:postcode'],
-				p['addr:city']
+				p.address,
+				p['addr:housenumber'], p.housenumber,
+				p['addr:street'], p.street,
+				p['addr:postcode'], p.postcode,
+				p['addr:city'], p.city
 			],
 			osmUrl = p.url = 'http://www.openstreetmap.org/' + (p.nodetype ? p.nodetype : 'node') + '/' + f.getId(),
 			popup = [
-				p.name ? '<b>' + p.name + '</b>' : '',
-				description ? description.charAt(0).toUpperCase() + description.substr(1) : '', // Uppercase the first letter
+				(p.name ? '<b>' + p.name + '</b>' : '') +
+				(p.alt_name ? '<b>' + p.alt_name + '</b>' : '') +
+				(p.short_name ? '<b>' + p.short_name + '</b>' : ''),
+				[(p.name || '').toLowerCase().match(language[p.tourism]) ? '' : p.tourism ? language[p.tourism] : p.tourism,
+					'*'.repeat(p.stars),
+					p.shelter_type == 'basic_hut' ? 'Abri' : '',
+					p.waterway == 'water_point' ? 'Point d\'eau' : '',
+					p.natural == 'spring' ? 'Source' : '',
+					p.man_made == 'water_well' ? 'Puits' : '',
+					language[p.amenity] ? language[p.amenity] : '',
+					p.rooms ? p.rooms + ' chambres' : '',
+					p.beds ? p.beds + ' lits' : '',
+					p.place ? p.place + ' places' : '',
+					p.capacity ? p.capacity + ' places' : '',
+					p.ele ? parseInt(p.ele, 10) + 'm' : '',
+				].join(' '),
 				phone ? '&phone;<a title="Appeler" href="tel:' + phone.replace(/[^0-9\+]+/ig, '') + '">' + phone + '</a>' : '',
 				p.email ? '&#9993;<a title="Envoyer un mail" href="mailto:' + p.email + '">' + p.email + '</a>' : '',
 				p['addr:street'] ? address.join(' ') : '',
 				p.website ? '&#8943;<a title="Voir le site web" target="_blank" href="' + p.website + '">' + (p.website.split('/')[2] || p.website) + '</a>' : '',
-				'&copy; Voir sur <a title="Voir la fiche d\'origine sur openstreetmap" target="_blank" href="' + osmUrl + '">OSM</a>'
+				p.opening_hours ? 'ouvert ' + p.opening_hours : '',
+				p.note ? p.note : ''
 			],
 			postLabel = typeof options.label == 'function' ? options.postLabel(p, f) : options.postLabel || '';
 
-		popup = popup.concat(typeof postLabel == 'object' ? postLabel : [postLabel]);
+		var internet = 0,
+			done = [
+				'geometry,lon,lat,area,amenity,building,highway,shop,shelter_type,access,waterway,natural,man_made',
+				'tourism,stars,rooms,place,capacity,ele,phone,contact,url,nodetype,name,alt_name,email,website',
+				'opening_hours,description,beds,bus,note',
+				'addr,housenumber,street,postcode,city,bus,public_transport,tactile_paving',
+				'ref,source,wheelchair,leisure,landuse,camp_site,bench,network,brand,bulk_purchase,organic',
+				'compressed_air,fuel,vending,vending_machine',
+				'fee,heritage,wikipedia,wikidata,operator,mhs,amenity_1,beverage,takeaway,delivery,cuisine',
+				'historic,motorcycle,drying,restaurant,hgv',
+				'drive_through,parking,park_ride,supervised,surface,created_by,maxstay'
+			].join(',').split(',');
+		for (var k in p) {
+			var k0 = k.split(':')[0];
+			if (!done.includes(k0))
+				switch (k0) {
+					case 'internet_access':
+						if (p[k] != 'no' && !internet++)
+							popup.push('Accès internet');
+						break;
+					default:
+						popup.push(k + ' : ' + p[k]);
+				}
+		}
+
+		popup = popup.concat(
+			typeof postLabel == 'object' ? postLabel : [postLabel], [
+				p.description,
+				'&copy; Voir sur <a title="Voir la fiche d\'origine sur openstreetmap" target="_blank" href="' + osmUrl + '">OSM</a>'
+			]
+		);
 		return ('<p>' + popup.join('</p><p>') + '</p>').replace(/<p>\s*<\/p>/ig, '');
 	}
 
@@ -622,16 +651,10 @@ function layerOverpass(options) {
 					var conditions = tags[t].split('"');
 					if (properties[conditions[1]] &&
 						properties[conditions[1]].match(conditions[3]))
-						return {
-							icon: checkElements[e].id,
-							name: properties[conditions[1]]
-						};
+						return checkElements[e].id;
 				}
 			}
-		return {
-			icon: 'inconnu',
-			name: 'inconnu'
-		};
+		return 'inconnu';
 	}
 
 	return layer;

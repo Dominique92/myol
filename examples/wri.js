a9be54746eeb452a5280cf4bdfe535abce57b3af
Dominique Cavailhez <dominique@c92.fr>
2024-02-15 20:39:29 +0100

    Fin refonte examples

--- examples/wri.js
+++ examples/wri.js
@@ -1,2 +1,5 @@
 document.body.appendChild(document.createElement('script')).src = 'vues/_cartes.js';
 document.body.appendChild(document.createElement('script')).src = 'vues/index.js';
+
+document.getElementById('example-title').innerHTML = 'Refuges.info';
+document.getElementById('example-next').href = '?wripoint';

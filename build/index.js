d68d3e5ed3bad2f8fe3d41627aeb44338cfbbf7e
Dominique Cavailhez <dominique@c92.fr>
2024-03-06 17:11:56 +0100

    Cosmetics

--- build/index.js
+++ build/index.js
@@ -1,6 +1,8 @@
 // This file defines the contents of the dist/myol.css & dist/myol libraries
 // This contains all what is necessary for refuges.info & chemineur.fr websites
 
+//TODO rename as dist.js
+
 import '../build/banner.css';
 import ol from '../src/ol'; // Openlayers native functions used in myol
 import myol from '../src'; // Map management pecific functions

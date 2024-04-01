e1b40f1e9397bbf0599689374cae9f5c1cfafd01
Dominique Cavailhez <dominique@c92.fr>
2024-04-01 21:29:53 +0200

    Refonte test swri -> cree point

---
+++ examples_wri/creepoint.js
@@ -0,0 +1,15 @@
+document.getElementById('example-title').innerHTML = 'WRI crée point ';
+
+/* global mapModif */
+const elScript = document.createElement('script');
+
+document.body.appendChild(elScript);
+elScript.src = '../examples_wri/vues/_cartes.js';
+elScript.addEventListener('load', () =>
+  mapModif({
+    host: 'https://www.refuges.info/', // '<?=$config_wri["sous_dossier_installation"]?>',
+    mapKeys: {}, //<?=json_encode($config_wri['mapKeys'])?>,
+    layerOptions: {}, //<?=json_encode($config_wri['layerOptions'])?>,
+    idPoint: 1234, // <?=intval($vue->point->id_point)?>;
+  })
+);
---
+++ examples_wri/point.js
@@ -0,0 +1,17 @@
+document.getElementById('example-title').innerHTML = 'WRI point';
+
+/* global mapPoint */
+const elScript = document.createElement('script');
+
+document.body.appendChild(elScript);
+elScript.src = '../examples_wri/vues/_cartes.js';
+elScript.addEventListener('load', () => {
+  const map = mapPoint({
+    host: 'https://www.refuges.info/', // '<?=$config_wri["sous_dossier_installation"]?>',
+    mapKeys: {}, //<?=json_encode($config_wri['mapKeys'])?>,
+    layerOptions: {}, //<?=json_encode($config_wri['layerOptions'])?>,
+  });
+
+  myol.trace(map);
+  return map;
+});

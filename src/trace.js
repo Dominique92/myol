3938d64a2e0d8c92339425375a2c1aefc1e49edf
Dominique Cavailhez <dominique@c92.fr>
2024-05-06 15:21:54 +0200

    Update trace.js

--- src/trace.js
+++ src/trace.js
@@ -3,7 +3,7 @@
  */
 
 import ol from './ol';
-import Geocoder from '@myol/geocoder/src/base';
+import Geocoder from '@myol/geocoder/src/base'; //TODO BUG to be replaced by ol-geocoder when /src published in npm
 /* global myol */
 
 export async function trace() {

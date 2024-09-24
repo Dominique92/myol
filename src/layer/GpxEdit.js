32f5b61707bcc06d2bc8fc351dc3de3f4fec2490
Dominique Cavailhez <dominique@c92.fr>
2024-09-24 20:29:17 +0200

    Merge optimizeCoordinates and optimiseAndSave

--- src/layer/GpxEdit.js
+++ src/layer/GpxEdit.js
@@ -44,7 +44,6 @@ function flatCoord(coords, splitCord) {
     } else
       lines.push(c);
   });
-  //TODO remove empty & 1 coord lines ??
 
   return lines;
 }
@@ -53,86 +52,6 @@ function flatFeatures(geom) {
   if (geom.getType().match(/collection/iu)) // Recurse Collections
     return geom.getGeometries().map(g => flatFeatures(g));
   return geom.getCoordinates();
-}
-
-// Refurbish Lines & Polygons
-function optimizeCoordinates(coordinates, options, splitCord) {
-  //TODO move into optimiseAndSave
-  // Get all edited features as array of lines coordinates
-  const lines = flatCoord(coordinates, splitCord),
-    polys = [];
-
-  // Merge lines having a common end
-  for (const a in lines) {
-    for (let b = 0; b < a; b++) { // Once each combination
-      if (lines[b]) {
-        const m = [a, b];
-
-        for (let i = 4; i; i--) // 4 times
-          if (lines[m[0]] && lines[m[1]]) { // Test if the line has been removed
-            // Shake lines end to explore all possibilities
-            m.reverse();
-            lines[m[0]].reverse();
-            if (compareCoords(lines[m[0]][lines[m[0]].length - 1], lines[m[1]][0])) {
-              // Merge 2 lines having 2 ends in common
-              lines[m[0]] = lines[m[0]].concat(lines[m[1]].slice(1)).reverse();
-              delete lines[m[1]]; // Remove the line but don't renumber the array keys
-            }
-          }
-      }
-    }
-  }
-
-  // Make polygons with looped lines
-  for (const a in lines)
-    if (options.editOnly !== 'line') {
-      // Close open lines
-      if (options.editOnly === 'poly')
-        if (!compareCoords(lines[a]))
-          lines[a].push(lines[a][0]);
-
-      if (compareCoords(lines[a])) { // If this line is closed
-        // Split squeezed polygons
-        // Explore all summits combinaison
-        for (let i1 = 0; i1 < lines[a].length - 1; i1++)
-          for (let i2 = 0; i2 < i1; i2++)
-            if (lines[a][i1][0] === lines[a][i2][0] &&
-              lines[a][i1][1] === lines[a][i2][1]) { // Find 2 identical summits
-              const squized = lines[a].splice(i2, i1 - i2); // Extract the squized part
-              squized.push(squized[0]); // Close the poly
-              polys.push([squized]); // Add the squized poly
-              i1 = lines[a].length; // End loop
-              i2 = lines[a].length;
-            }
-
-        // Convert closed lines to polygons
-        polys.push([lines[a]]); // Add the polygon
-        delete lines[a]; // Forget the line
-      }
-    }
-
-  // Makes holes if a polygon is included in a biggest one
-  for (const p1 in polys) // Explore all Polygons combinaison
-    if (options.withHoles && polys[p1]) {
-      const fs = new ol.geom.Polygon(polys[p1]);
-
-      for (const p2 in polys)
-        if (polys[p2] && p1 !== p2) {
-          let intersects = true;
-          for (const c in polys[p2][0])
-            if (!fs.intersectsCoordinate(polys[p2][0][c]))
-              intersects = false;
-          if (intersects) { // If one intersects a bigger
-            polys[p1].push(polys[p2][0]); // Include the smaler in the bigger
-            delete polys[p2]; // Forget the smaller
-          }
-        }
-    }
-
-  return {
-    lines: lines.filter(Boolean), // Remove deleted array members
-    polys: polys.filter(Boolean),
-  };
 }
 
 // STYLES
@@ -347,33 +266,124 @@ class GpxEdit extends VectorLayer {
     });
   } // End setMapInternal
 
+  restartInteractions(intName) {
+    this.map.getTargetElement().firstChild.className = 'ol-viewport ed-view-' + intName;
+
+    ['select', 'modify', 'draw', 'snap'].forEach(i =>
+      this.map.removeInteraction(this[i + 'Interaction'])
+    );
+
+    if (intName === 'modify')
+      this.map.addInteraction(this.selectInteraction);
+
+    this.map.addInteraction(this[intName + 'Interaction']);
+    this.map.addInteraction(this.snapInteraction); // Must be added after the others
+
+    // For snap & traceSource : register again the full list of features as addFeature manages already registered
+    this.snapSource.clear();
+    this.map.getLayers().forEach(layer => {
+      if (layer.getSource() !== this.editedSource &&
+        layer.getSource() &&
+        layer.getSource().getFeatures) // Vector layers only
+        layer.getSource().getFeatures().forEach(feature => {
+          this.snapInteraction.addFeature(feature);
+          this.snapSource.addFeature(feature);
+        });
+    });
+  }
+
   optimiseAndSave(splitCord) {
     // Get optimized coords
     const editedFeatures = this.editedSource.getFeatures(), // Get edited features
       coordinates = editedFeatures.map(
         f => flatFeatures(f.getGeometry()) // Get flat coordinates
-      ),
-      optCoords = optimizeCoordinates(
-        coordinates,
-        this.options,
-        splitCord,
       );
+    // Get all edited features as array of lines coordinates
+    const lines = flatCoord(coordinates, splitCord),
+      polys = [];
+
+    // Merge lines having a common end
+    for (const a in lines) {
+      for (let b = 0; b < a; b++) { // Once each combination
+        if (lines[b]) {
+          const m = [a, b];
+
+          for (let i = 4; i; i--) // 4 times
+            if (lines[m[0]] && lines[m[1]]) { // Test if the line has been removed
+              // Shake lines end to explore all possibilities
+              m.reverse();
+              lines[m[0]].reverse();
+              if (compareCoords(lines[m[0]][lines[m[0]].length - 1], lines[m[1]][0])) {
+                // Merge 2 lines having 2 ends in common
+                lines[m[0]] = lines[m[0]].concat(lines[m[1]].slice(1)).reverse();
+                delete lines[m[1]]; // Remove the line but don't renumber the array keys
+              }
+            }
+        }
+      }
+    }
+
+    // Make polygons with looped lines
+    for (const a in lines)
+      if (this.options.editOnly !== 'line') {
+        // Close open lines
+        if (this.options.editOnly === 'poly')
+          if (!compareCoords(lines[a]))
+            lines[a].push(lines[a][0]);
+
+        if (compareCoords(lines[a])) { // If this line is closed
+          // Split squeezed polygons
+          // Explore all summits combinaison
+          for (let i1 = 0; i1 < lines[a].length - 1; i1++)
+            for (let i2 = 0; i2 < i1; i2++)
+              if (lines[a][i1][0] === lines[a][i2][0] &&
+                lines[a][i1][1] === lines[a][i2][1]) { // Find 2 identical summits
+                const squized = lines[a].splice(i2, i1 - i2); // Extract the squized part
+                squized.push(squized[0]); // Close the poly
+                polys.push([squized]); // Add the squized poly
+                i1 = lines[a].length; // End loop
+                i2 = lines[a].length;
+              }
+
+          // Convert closed lines to polygons
+          polys.push([lines[a]]); // Add the polygon
+          delete lines[a]; // Forget the line
+        }
+      }
+
+    // Makes holes if a polygon is included in a biggest one
+    for (const p1 in polys) // Explore all Polygons combinaison
+      if (this.options.withHoles && polys[p1]) {
+        const fs = new ol.geom.Polygon(polys[p1]);
+
+        for (const p2 in polys)
+          if (polys[p2] && p1 !== p2) {
+            let intersects = true;
+            for (const c in polys[p2][0])
+              if (!fs.intersectsCoordinate(polys[p2][0][c]))
+                intersects = false;
+            if (intersects) { // If one intersects a bigger
+              polys[p1].push(polys[p2][0]); // Include the smaler in the bigger
+              delete polys[p2]; // Forget the smaller
+            }
+          }
+      }
 
     // Body class to handle edit polys only
-    if (!optCoords.lines.length && optCoords.polys.length)
+    if (!lines.length && polys.length)
       document.body.classList.add('edit-only-polys');
     else
       document.body.classList.remove('edit-only-polys');
 
     // Recreate features
     this.editedSource.clear();
-    optCoords.lines.forEach(line => {
+    lines.forEach(line => {
       this.editedSource.addFeature(new Feature({
         geometry: new ol.geom.LineString(line),
         //BEST "full.geojson" Multilinestring / Multipolygon
       }));
     });
-    optCoords.polys.forEach(poly => {
+    polys.forEach(poly => {
       this.editedSource.addFeature(new Feature({
         geometry: new ol.geom.Polygon(poly),
       }));
@@ -412,32 +422,6 @@ class GpxEdit extends VectorLayer {
       }
     });
   } // End optimiseAndSave
-
-  restartInteractions(intName) {
-    this.map.getTargetElement().firstChild.className = 'ol-viewport ed-view-' + intName;
-
-    ['select', 'modify', 'draw', 'snap'].forEach(i =>
-      this.map.removeInteraction(this[i + 'Interaction'])
-    );
-
-    if (intName === 'modify')
-      this.map.addInteraction(this.selectInteraction);
-
-    this.map.addInteraction(this[intName + 'Interaction']);
-    this.map.addInteraction(this.snapInteraction); // Must be added after the others
-
-    // For snap & traceSource : register again the full list of features as addFeature manages already registered
-    this.snapSource.clear();
-    this.map.getLayers().forEach(layer => {
-      if (layer.getSource() !== this.editedSource &&
-        layer.getSource() &&
-        layer.getSource().getFeatures) // Vector layers only
-        layer.getSource().getFeatures().forEach(feature => {
-          this.snapInteraction.addFeature(feature);
-          this.snapSource.addFeature(feature);
-        });
-    });
-  }
 }
 
 export default GpxEdit;

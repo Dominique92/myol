b868a9635b86511d831b42f117ac7ab5caf9bfd1
Dominique Cavailhez <2606683+Dominique92@users.noreply.github.com>
2025-11-18 11:49:05 +0100

    tests en cours

--- examples/pwa/map.js
+++ examples/pwa/map.js
@@ -18,8 +18,12 @@ function tiledBboxStrategy(extent, resolution) {
   return tiledExtents;
 }
 
-/* eslint-disable-next-line no-unused-vars */
-const map = new ol.Map({
+const points= new myol.layer.vector.WRI({
+      strategy: tiledBboxStrategy,
+      //debug: true,
+    }),
+    
+map = new ol.Map({
   target: 'map',
 
   view: new ol.View({
@@ -41,12 +45,49 @@ const map = new ol.Map({
     }),
 
     // Vector layers
-    new myol.layer.vector.WRI({
-      strategy: tiledBboxStrategy,
-      debug: true,
-    }),
+   points,
 
     // Hover & click management (mouse & touch)
     new myol.layer.Hover(),
   ],
 });
+
+    /* eslint-disable-next-line no-unused-vars */
+function tileNumber(lat_deg, lon_deg, zoom){
+const	lat_rad = lat_deg* (Math.PI / 180),
+	n = 2.0 ** zoom,
+	xtile = Math.round((lon_deg + 180.0) / 360.0 * n),
+	ytile = Math.round((1.0 - Math.log(Math.tan(lat_rad) + (1 / Math.cos(lat_rad))) / Math.PI) / 2.0 * n);
+  
+  //*DCMM*/console.log([xtile, ytile, zoom]);
+
+	return [zoom,xtile, ytile];
+}
+
+  // Compute properties when the layer is loaded & before the cluster layer is computed
+      points.on('change', () => {
+        
+        const center4326=ol.proj.transform(map.getView().getCenter(), 'EPSG:3857', 'EPSG:4326');
+        if(0)
+        /*DCMM*/console.log(tileNumber(
+        center4326[0],
+        center4326[1],
+        map.getView().getZoom()
+        ));
+        //*DCMM*/console.log(center4326);
+
+//*DCMM*/console.log(map.getView().getZoom());
+        
+        
+  /*      this.logs ??= {};
+        //if (this.options.debug)
+          if(0)
+          console.info(
+            'Receive 1 tile XXX ' +
+            this.logs.tileSize + ', ' +// this.getFeatures().length +
+            //(this.logs.isCluster ? ' clusters, ' : ' points, ') +
+            ol.proj.transform(map.getView().getCenter(this.getExtent()), 'EPSG:3857', 'EPSG:4326')
+            .map(x => Math.round(x * 1000) / 1000)
+            .join('°E/') + '°N'
+          );*/
+      });

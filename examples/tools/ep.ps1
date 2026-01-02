fa1ca99aacf0e4ef37b09a985ee7e495346055e1
Dominique Cavailhez <rando.gv92@gmail.com>
2026-01-02 14:59:28 +0100

    -fe / +tools

---
+++ examples/tools/ep.ps1
@@ -0,0 +1,2 @@
+cd "C:\Program Files\nodejs\dom"
+Set-ExecutionPolicy -ExecutionPolicy AllSigned -scope MachinePolicy
---
+++ examples/tools/ub.ps1
@@ -0,0 +1,2 @@
+Set-Location C:\Users\$Env:UserName\Documents\GitHub\
+ls -r | Unblock-File
